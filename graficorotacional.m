025de0e257c7c7f409e630ac4192a03ab6bf05df
DAVO-555 <157550093+DAVO-555@users.noreply.github.com>
2024-02-20 16:42:31 -0600

    Add files via upload

---
+++ graficorotacional.m
@@ -0,0 +1,15 @@
+[t,x]=ode45(@movrotacional,[0 10], [0 2]);
+
+figure(1)
+plot(t,x(:,1),'b');
+grid on
+title("Posición (MATLAB)");
+xlabel("Tiempo");
+ylabel("Posición");
+
+figure(2)
+plot(t,x(:,2),'r');
+grid on
+title("Velocidad (MATLAB)");
+xlabel("Tiempo");
+ylabel("Velocidad");
---
+++ movimientorvalores.m
@@ -0,0 +1,5 @@
+m = 10;
+k = 100;
+r = 0.05;
+
+sim("movimientorsimu.slx")
